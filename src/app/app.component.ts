ea2fdb728d1bb78dff95677fa5bb4cdac8055f95
Antonio Araujo <anthenriqufrj@gmail.com>
2018-08-15 22:24:08 -0300

    inital design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'minha-comanda';
+  displayedColumns: string[] = ['produto', 'preco', 'quantidade', 'total'];
+  dataSource = [{
+    produto: 'Cerveja',
+    preco: 6.00,
+    quantidade: 3,
+    getTotal: function () { return this.preco * this.quantidade }
+  },{
+    produto: 'Chopp',
+    preco: 5.00,
+    quantidade: 2,
+    getTotal: function () { return this.preco * this.quantidade }
+  },{
+    produto: 'Pizaa',
+    preco: 8,
+    quantidade: 1,
+    getTotal: function () { return this.preco * this.quantidade }
+  }]
+
+  getPrecoTotal() {
+    return this.dataSource.map(t => t.getTotal()).reduce((acc, value) => acc + value, 0);
+  }
 }
